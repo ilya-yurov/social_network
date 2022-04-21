f6fa20d8f91e327e311e3f08372d6a8325fd7cb5
Ilya Yurov <ilya_yurov_96@mail.ru>
2022-04-22 00:23:41 +1000

    09. Separated UI and BLL(data) from components. Make arrays of JSX component from DLL (data) by map( )

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,28 +1,36 @@
-import { NavLink } from 'react-router-dom';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 import s from './Dialogs.module.scss'
+
+let dialogs = [
+	{name: 'Victor', id: 1, active: 0},
+	{name: 'Nadya', id: 2, active: 0},
+	{name: 'Irina', id: 3, active: 1},
+	{name: 'Ilya', id: 4, active: 0},
+	{name: 'John', id: 5, active: 0},
+	{name: 'Petya', id: 6, active: 0},
+	{name: 'Valentina', id: 7, active: 0}
+]
+let messages = [
+	{message: 'Hi there!', id: 1, active: 0},
+	{message: 'How are you?', id: 2, active: 0},
+	{message: 'Wanna walk outside?', id: 3, active: 1},
+	{message: 'I\'m so sad :(', id: 4, active: 0},
+	{message: ':D', id: 5, active: 0},
+	{message: 'Hmm... just ignore you...', id: 6, active: 0},
+	{message: 'Californicaaaaaaation!!!', id: 7, active: 0}
+]
+let dialogElements = dialogs.map( (d) => <Dialog name={d.name} id={d.id} active={d.active}/>)
+let messageElements = messages.map( (m) => <Message  message={m.message} id={m.id} active={m.active}/>)
 
 const Dialogs = (props) => {
 	return (
 		<div className={s.content}>
 			<div className={s.content__dialogs}>
-				<Dialog name="Victor" id="1" />
-				<Dialog name="Nadya" id="2" />
-				<Dialog name="Irina" id="3" active="1" />
-				<Dialog name="Ilya" id="4" />
-				<Dialog name="John" id="5" />
-				<Dialog name="Petya" id="6" />
-				<Dialog name="Valentina" id="7" />
+				{dialogElements}
 			</div>
 			<div className={s.content__messages}>
-				<Message message="Hi there!" />
-				<Message message="How are you?" />
-				<Message message="OMG!" active="1" />
-				<Message message="Wanna walk outside?"  />
-				<Message message="I'm so sad :(" />
-				<Message message=":D" />
-				<Message message="Hmm... just ignore you..." />
+				{messageElements}
 			</div>
 		</div>
 	)
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import Input from './Input/Input';
 import s from './MyPosts.module.scss'
 import Post from './Post/Post';
 
+let posts = [
+	{message: "Hello, that's my first try of props usage!", id: 1, likes: "10"},
+	{message: "I wanna have 99 likes at this post!", id: 2, likes: "99"},
+	{message: "OMG he did it!!!", id: 3, likes: "999"}
+]
+
+let postElements = posts.map((p) => <Post message={p.message} id={p.id} likes ={p.likes} />)
 
 const MyPosts = () => {
 	return (
@@ -12,12 +19,10 @@ const MyPosts = () => {
 				<Input />
 			</div>
 			<div className={s.content__postWrapper}>
-				<Post message="Hello, that's my first try of props usage!" likes = "10" />
-				<Post message="I wanna have 99 likes at this post!" likes = "99" />
-				<Post message="OMG he did it!!!" likes = "999" />
+				{postElements}
 			</div>
 		</div>
 	);
 }
-
+  
 export default MyPosts;
