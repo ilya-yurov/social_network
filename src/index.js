9c85ef4d006d3b48e34bd2be28b4b05d7702f570
Ilya Yurov <ilya_yurov_96@mail.ru>
2022-08-07 12:59:18 +1000

    changed BrowserRouter to HashRouter to be able to update the page on Github Pages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css'
 import App from './App'
 import store from './redux/redux-store'
 import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter>
 			<Provider store={store}>
 				<App state={store.getState()} store={store} />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
